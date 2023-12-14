65295a07a1ed50480195dd48062e048728b221a2
Bhavik Thakkar <thakkarb97@gmail.com>
2023-12-14 08:25:19 +0530

    Add more code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bhavik29/puppy
 
 go 1.21.5
+
+require github.com/bhavik29/dog v0.0.0-20231214024801-1e942f8f5db9
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/bhavik29/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func WhenGrownUp() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func WhenGrownUps() string {
+	return dog.WhenGrownUp(Barks())
 }
